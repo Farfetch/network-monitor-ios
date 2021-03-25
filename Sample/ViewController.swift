89ec746fe27aed69f72b42dd583137560e119da0
Diogo Balseiro <diogo.balseiro@farfetch.com>
2021-03-25 15:21:10 +0000

    refactor(main): Tweaked sample app UI margins

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -13,8 +13,8 @@ final class ViewController: UIViewController {
 
     enum Constants {
 
-        static let labelTopPadding: CGFloat = 50.0
-        static let imageSidePadding: CGFloat = 10.0
+        static let labelTopPadding: CGFloat = 60.0
+        static let imageSidePadding: CGFloat = 15.0
         static let imageBorderWidth: CGFloat = 3.0
 
         static let robotURL = "https://www.alphabet.com/robots.txt"
